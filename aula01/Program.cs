d241338ba188397d56d6fa561ee1cd2f3df9ead2
LucasTeodoro2003 <68308060+LucasTeodoro2003@users.noreply.github.com>
2024-08-13 23:11:48 +0000

    Rodou Primeira vez

--- aula01/Program.cs
+++ aula01/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Proram
+{
+    public static void Main(string[] args)
+    {
+        // Cadastrar dados Pessoa
+        Console.WriteLine("Cadastro de Pessoa! \n");
+        Console.Write("Nome:");
+        string nomePessoa = Console.ReadLine();
+        Console.Write("Idade:");
+        var idadePessoa = int.Parse(Console.ReadLine());
+
+        Pessoa pessoa1 = new Pessoa(nomePessoa, idadePessoa);
+        Console.WriteLine("Pessoa Cadastrada com sucesso! \n");
+
+        Console.WriteLine(pessoa1.ToString());
+    }
+}
--- aula01/obj/Debug/net8.0/aula01.AssemblyInfo.cs
+++ aula01/obj/Debug/net8.0/aula01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cd4248e4e0eb665d89555e7f6d416ec53dc381e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d6ab9aa103957e756859ff8dda0ac11f793c07e")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
