3573a9cd48c927bc3c2cbc28feeb24d7af94cc7c
LucasTeodoro2003 <68308060+LucasTeodoro2003@users.noreply.github.com>
2024-08-20 23:28:50 +0000

    Aula 03

---
+++ aula04/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Pessoa objPessoa = new Pessoa("Lucas");
+        PessoaStruct objPessoaStruct = new PessoaStruct("Lucas Struct");
+        Console.WriteLine($@"Antes do método (Classe):
+                        {objPessoa.ToString()}");
+        // Console.WriteLine("Antes do método (Classe):" + objPessoa.ToString());
+        alterarNomeClasse(objPessoa);
+        Console.WriteLine($@"Depois do método (Classe):
+                        {objPessoa.ToString()}");      
+
+        // Struct                                 
+        Console.WriteLine($@"Antes do método (Struct):
+                        {objPessoaStruct.ToString()}");
+        alterarNomeClasseStruct(objPessoaStruct);
+        Console.WriteLine($@"Antes do método (Struct):
+                        {objPessoaStruct.ToString()}");
+    }
+
+    public static void alterarNomeClasse(Pessoa objAux)
+    {
+        objAux.Nome = "Willian";
+    }
+
+    public static void alterarNomeClasseStruct(PessoaStruct objAux){
+        objAux.NomeStruct = "Willian Struct";
+        Console.WriteLine($"\n \n Dentro do método Struct: {objAux.NomeStruct}\n\n");
+    }
+}
