a7f8e1b3df3cdfb72fadb0aefda0177b8f1e41a0
LucasTeodoro2003 <lucasteodoro.lm@gmail.com>
2024-08-20 09:37:36 -0300

    updateAula03

--- aula01/obj/Debug/net8.0/aula01.AssemblyInfo.cs
+++ aula01/obj/Debug/net8.0/aula01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d6ab9aa103957e756859ff8dda0ac11f793c07e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c8f282b8d8a5eb43cbee684cae191ab31152d4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
