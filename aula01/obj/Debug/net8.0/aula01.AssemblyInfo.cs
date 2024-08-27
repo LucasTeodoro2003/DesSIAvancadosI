08866359706aea452ee018937614f83090d119db
LucasTeodoro2003 <68308060+LucasTeodoro2003@users.noreply.github.com>
2024-08-27 00:29:46 +0000

    Aula05

--- aula01/obj/Debug/net8.0/aula01.AssemblyInfo.cs
+++ aula01/obj/Debug/net8.0/aula01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7f8e1b3df3cdfb72fadb0aefda0177b8f1e41a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2feb382bde342a8d5e4cf090d18d556d0e6675d")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ aula05/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program{
+    public static void Main(string[] args){
+        //Conversao implicita
+        int numInt = 45;
+        double numDouble = numInt;
+        Console.WriteLine("Conversao implicita de int para double: ");
+        Console.WriteLine($"Int: {numInt}, Double: {numDouble}");
+
+
+        //Conversao eusando o Convert
+        double valorDouble = 7.45;
+        int valorTeste = 8;
+        int valorConvertidoInt = Convert.ToInt32(valorDouble);
+        string textoValor = "123";
+        int textoConvertidoInt = Convert.ToInt32(textoValor);
+        Console.WriteLine($"Double: {valorDouble}, Inteiro: {valorConvertidoInt}, String: {textoValor}, Inteiro: {textoConvertidoInt}");
+        Console.WriteLine($"{(int)valorDouble}, {(double)valorTeste}");
+    }
+}
