dd91f219d6faaf1dfd01e43faf0ff96d624a5c01
LucasTeodoro2003 <68308060+LucasTeodoro2003@users.noreply.github.com>
2024-09-10 23:27:49 +0000

    aula09

---
+++ aula09/Compra.cs
@@ -0,0 +1,25 @@
+﻿public class Compra
+{
+    public Cliente Cliente { get; set; }
+    public List<Produto> Produtos { get; set; }
+
+
+    public Compra(Cliente cliente){
+        Cliente = cliente;
+        Produtos = new List<Produto>();
+
+    }
+
+    public void AdcionarProduto(Produto produto){
+        Produtos.Add(produto);
+    }
+
+    public decimal CalcularTotal(){
+        return Produtos.Sum(p => p.Preco);
+    }
+
+    public override string ToString(){
+        string produtoStr = string.Join(",", Produtos.Select(p => p.Nome));
+        return $"Compra de {Cliente.Nome}, Produtos: [{produtoStr}], Total: {CalcularTotal():C}";
+    }
+}
---
+++ aula09/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+
+        //Criando Clientes
+        Cliente cliente1 = new Cliente(1, "João");
+        Cliente cliente2 = new Cliente(2, "Jorge");
+        Cliente cliente3 = new Cliente(3, "Thiago");
+        Cliente cliente4 = new Cliente(4, "Ruan");
+        Cliente cliente5 = new Cliente(5, "Juan");
+        Cliente cliente6 = new Cliente(6, "Bia");
+        Cliente cliente7 = new Cliente(7, "Guilherme");
+        Cliente cliente8 = new Cliente(8, "Alemão");
+
+
+        ///Criando Produtos
+        Produto produto1 = new Produto(1, "Notebook", 3500.00M);
+        Produto produto2 = new Produto(2, "Notebook Positivo", 10.00M);
+        Produto produto3 = new Produto(3, "Pendrive Kill", 4000.00M);
+        Produto produto4 = new Produto(4, "Celular Positivo", 8.00M);
+        Produto produto5 = new Produto(5, "Fone de Ouvido", 15.00M);
+        Produto produto6 = new Produto(6, "Tablet", 1200.00M);
+
+
+        // Criando compra para cliente 1
+        Compra compra1 = new Compra(cliente1);
+        compra1.AdcionarProduto(produto1);
+        compra1.AdcionarProduto(produto3);
+        compra1.AdcionarProduto(produto5);
+        compra1.AdcionarProduto(produto6);
+
+        // Criando compra para cliente 2
+        Compra compra2 = new Compra(cliente2);
+        compra2.AdcionarProduto(produto2);
+        compra2.AdcionarProduto(produto4);
+        compra2.AdcionarProduto(produto4);
+        compra2.AdcionarProduto(produto3);
+
+
+        // Exibir as compras
+
+        Console.WriteLine(compra1);
+        Console.WriteLine(compra2);
+
+    }
+}
