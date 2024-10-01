d7fe4b710dfcfb82c2627bd7bb5f06edd713af51
LucasTeodoro2003 <68308060+LucasTeodoro2003@users.noreply.github.com>
2024-10-01 19:18:11 +0000

    atividadeAvaliativa

--- aula05/obj/Debug/net8.0/aula05.AssemblyInfo.cs
+++ aula05/obj/Debug/net8.0/aula05.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+468bcfaeb5dabbbd4e15a637868bf8855b51be64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4c9fa8002145c8954c3f30c9718bc4b431df11c")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
